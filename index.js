d8e4df443200fb9a8082cf818653d9fdc96c54be
Anderson Sanches <andycds@gmail.com>
2023-11-23 21:27:18 -0300

    Funcionando

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.post('/pergunte-ao-chatgpt', (req, res) => {
+app.post('/pergunte-ao-chatgpt', async (req, res) => {
     const { prompt } = req.body;
-    console.log(prompt);
-    res.json({ seuPrompt: prompt });
+    const model = 'gpt-3.5-turbo';
+    const role = 'user';
+    const max_tokens = 50;
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: role, content: prompt }],
+        model: model,
+        max_tokens: max_tokens
+    });
+    res.json({ completion: completion.choices[0].message.content });
 });
 
 app.listen(4000, () => console.log('ChatGPT_Backend em execução na porta 4000'));
